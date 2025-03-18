061fcc1eb5083352c516dc1aef5476c85903947d
Sariah <sariahwandler@gmail.com>
2025-03-18 17:10:30 -0600

    MVC start

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -23,9 +25,9 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 //Index Route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome);
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
